e6bf14b64495989e318b090be07f00b082338fab
Seb Insua <me@sebinsua.com>
2021-10-05 22:53:29 +0100

    Add support for oembedding tweets

--- lib/renderMarkdown.ts
+++ lib/renderMarkdown.ts
@@ -1,5 +1,6 @@
 import unified from "unified";
 import remarkParse from "remark-parse";
+import remarkOembed from "remark-oembed";
 import remarkShikiTwoSlash from "remark-shiki-twoslash";
 import rehype from "remark-rehype";
 import rehypeRaw from "rehype-raw";
@@ -10,6 +11,7 @@ import rehypeStringify from "rehype-stringify";
 const render = (markdown: string) =>
   unified()
     .use(remarkParse)
+    .use(remarkOembed)
     .use(remarkShikiTwoSlash, {
       theme: require("../styles/monochrome-dark.json"),
     })
--- posts/hello-world.md
+++ posts/hello-world.md
@@ -5,3 +5,5 @@ date: "2021-09-28"
 ---
 
 Hello world.
+
+https://twitter.com/jessitron/status/1255850505798025218
