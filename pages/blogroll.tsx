120b8d16ed70e5c1ae7cd5e86d4a1abe369957c6
Seb Insua <me@sebinsua.com>
2023-03-13 13:05:45 +0000

    Add further blog posts

--- pages/blogroll.tsx
+++ pages/blogroll.tsx
@@ -57,31 +57,10 @@ export default function BlogrollPage() {
             by <a href="https://dominictarr.com/">Dominic Tarr</a>.
           </li>
           <li>
-            <a href="https://two-wrongs.com/data-consistency-is-overrated.html">
-              <em>“Data Consistency Is Overrated”</em> (2023)
+            <a href="https://jessitron.com/2021/01/18/when-costs-are-nonlinear-keep-it-small/">
+              <em>“When costs are nonlinear, keep it small”</em> (2021)
             </a>{" "}
-            by <a href="https://xkqr.org/profile">Christoffer Stjernlöf</a>.
-          </li>
-          <li>
-            <a href="https://jvns.ca/blog/2015/04/14/strace-zine/">
-              <em>
-                How to spy on your programs with <pre>strace</pre>
-              </em>{" "}
-              (2015)
-            </a>{" "}
-            by <a href="https://jvns.ca/">Julia Evans</a>.
-          </li>
-          <li>
-            <a href="https://danluu.com/file-consistency/">
-              <em>“Files are hard”</em> (2017)
-            </a>{" "}
-            by <a href="https://danluu.com/">Dan Luu</a>.
-          </li>
-          <li>
-            <a href="https://wordsandbuttons.online/programmers_guide_to_polynomials_and_splines.html">
-              <em>“Programmer’s guide to polynomials and splines”</em> (2019)
-            </a>{" "}
-            by <a href="https://wordsandbuttons.online/">Oleksandr Kaleniuk</a>.
+            by <a href="https://jessitron.com/">Jessica Kerr</a>.
           </li>
           <li>
             <a href="https://sophiebits.com/2018/12/03/yak-shaving-fixing.html">
@@ -96,10 +75,91 @@ export default function BlogrollPage() {
             by <a href="https://sandimetz.com/">Sandi Metz</a>.
           </li>
           <li>
+            <a href="https://wordsandbuttons.online/programmers_guide_to_polynomials_and_splines.html">
+              <em>“Programmer’s guide to polynomials and splines”</em> (2019)
+            </a>{" "}
+            by <a href="https://wordsandbuttons.online/">Oleksandr Kaleniuk</a>.
+          </li>
+          <li>
+            <a href="https://chidiwilliams.com/post/quadtrees/">
+              <em>“Quadtrees in the Wild”</em> (2021)
+            </a>{" "}
+            by <a href="https://chidiwilliams.com/">Chidi Williams</a>.
+          </li>
+          <li>
+            <a href="https://two-wrongs.com/data-consistency-is-overrated.html">
+              <em>“Data Consistency Is Overrated”</em> (2023)
+            </a>{" "}
+            by <a href="https://xkqr.org/profile">Christoffer Stjernlöf</a>.
+          </li>
+          <li>
+            <a href="https://jvns.ca/blog/2015/04/14/strace-zine/">
+              <em>
+                “How to spy on your programs with <pre>strace</pre>”
+              </em>{" "}
+              (2015)
+            </a>{" "}
+            by <a href="https://jvns.ca/">Julia Evans</a>.
+          </li>
+          <li>
+            <a href="https://tiarkrompf.github.io/notes/?/just-write-the-parser/">
+              <em>“Just write the #!%/* parser”</em> (2019)
+            </a>{" "}
+            by <a href="https://tiarkrompf.github.io/">Tiark Rompf</a>.
+          </li>
+          <li>
+            <a href="https://danluu.com/file-consistency/">
+              <em>“Files are hard”</em> (2017)
+            </a>{" "}
+            by <a href="https://danluu.com/">Dan Luu</a>.
+          </li>
+          <li>
             <a href="https://lethain.com/learn-to-never-be-wrong/">
               <em>“Learn to never be wrong”</em> (2020)
             </a>{" "}
             by <a href="https://lethain.com/">Will Larson</a>.
+          </li>
+          <li>
+            <a href="https://macwright.com/2021/02/17/the-naming-of-things.html">
+              <em>
+                “Picking better names for variables, functions, and projects”
+              </em>{" "}
+              (2021)
+            </a>{" "}
+            by <a href="https://macwright.com/">Tom MacWright</a>.
+          </li>
+          <li>
+            <a href="https://monkey.org/~marius/unix-tools-hints.html">
+              <em>“Hints for writing Unix tools”</em> (2014)
+            </a>{" "}
+            by <a href="https://monkey.org/~marius/">Marius Eriksen</a>.
+          </li>
+          <li>
+            <a href="https://lord.io/spreadsheets/">
+              <em>“How to recalculate a spreadsheet”</em> (2020)
+            </a>{" "}
+            by <a href="https://lord.io/">Robert Lord</a>.
+          </li>
+          <li>
+            <a href="https://koolaidfactory.com/writing-in-public-inside-your-company/">
+              <em>“Writing In Public, Inside Your Company”</em> (2021)
+            </a>{" "}
+            by <a href="https://www.briewolfson.com">Brie Wolfson</a>.
+          </li>
+          <li>
+            <a href="https://www.patrickstevens.co.uk/posts/2021-02-20-in-praise-of-dry-run/">
+              <em>
+                “In praise of <pre>--dry-run</pre>”
+              </em>{" "}
+              (2021)
+            </a>{" "}
+            by <a href="https://www.patrickstevens.co.uk">Patrick Stevens</a>.
+          </li>
+          <li>
+            <a href="https://mrale.ph/blog/2015/01/11/whats-up-with-monomorphism.html">
+              <em>“What’s up with monomorphism?”</em> (2015)
+            </a>{" "}
+            by <a href="https://mrale.ph/">Vyacheslav Egorov</a>.
           </li>
         </ul>
       </div>
