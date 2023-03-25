79f918c89f8753fd02e4815e244923408efbc187
Seb Insua <me@sebinsua.com>
2023-03-25 17:48:28 +0000

    Only export when NEXT_EXPORT is set to true

--- next.config.js
+++ next.config.js
@@ -4,7 +4,6 @@ const withLinaria = require("next-with-linaria");
  * @type {import('next-with-linaria').LinariaConfig}
  */
 const config = {
-  output: "export",
   experimental: {
     appDir: true,
     typedRoutes: true,
@@ -15,5 +14,9 @@ const config = {
   reactStrictMode: true,
   swcMinify: true,
 };
+
+if (process.env.NEXT_EXPORT === "true") {
+  config.output = "export";
+}
 
 module.exports = withLinaria(config);
