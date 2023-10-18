70ca77bc351a0f450292361935f670c54c95fbfd
Seb Insua <me@sebinsua.com>
2023-10-18 14:08:30 +0100

    Make the profile image a client component so it can fade in

--- app/about/ProfileImage.tsx
+++ app/about/ProfileImage.tsx
@@ -1,7 +1,27 @@
+"use client";
+
+import { useRef, useEffect } from "react";
+
 export function ProfileImage() {
-  // We're dropping the `next/image` as it isn't supported in Cloudflare anyway,
-  // and we end up having a client component due to the `loader` property we require
-  // for our custom usage.
+  const imageRef = useRef<HTMLImageElement>(null);
+  useEffect(() => {
+    const image = imageRef.current!;
+    function handleImageLoaded() {
+      image.style.opacity = "1.0";
+    }
+
+    if (image.complete) {
+      handleImageLoaded();
+      return;
+    }
+
+    image.addEventListener("load", handleImageLoaded);
+    return () => {
+      image.removeEventListener("load", handleImageLoaded);
+    };
+  }, []);
+
+  // We're dropping the `next/image` as it isn't supported in Cloudflare anyway.
   //
   // <Image
   //   src="/assets/seb-insua.jpg"
@@ -15,13 +35,18 @@ export function ProfileImage() {
     <>
       {/* eslint-disable-next-line @next/next/no-img-element */}
       <img
+        ref={imageRef}
         src="/assets/seb-insua.jpg"
         alt="Seb Insua, Japan (2019)"
         width={800 * 0.72}
         height={603 * 0.72}
         loading="lazy"
         decoding="async"
-        style={{ color: "transparent" }}
+        style={{
+          color: "transparent",
+          opacity: 0,
+          transition: "opacity 0.3s ease-in-out",
+        }}
       />
     </>
   );
